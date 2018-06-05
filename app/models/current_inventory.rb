00ca7fabb74a48f639ade3f51c0f898f145679f2
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-06-05 15:09:40 +0530

    added export

--- app/models/current_inventory.rb
+++ app/models/current_inventory.rb
@@ -2,4 +2,14 @@ class CurrentInventory < ApplicationRecord
   belongs_to :inward_product , optional: :true
   belongs_to :delivery_inward , optional: :true
   belongs_to :product , optional: :true
+
+      def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |return_module|
+        csv << return_module.attributes.values_at(*column_names)
+      end
+    end
+end
+
 end
