26b1c90889958c9c928c4c03cec93381e120750e
komya-21 <komalmane21@gmail.com>
2018-08-01 11:15:54 +0530

    added assets.rb

---
+++ config/initializers/assets.rb
@@ -0,0 +1,52 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( home.js )
+Rails.application.config.assets.precompile += %w( companies.css )
+Rails.application.config.assets.precompile += %w( companies.js )
+Rails.application.config.assets.precompile += %w( current_inventories.css )
+Rails.application.config.assets.precompile += %w( current_inventories.js )
+Rails.application.config.assets.precompile += %w( dashboard.css )
+Rails.application.config.assets.precompile += %w( dashboard.js )
+Rails.application.config.assets.precompile += %w( deliveries.css )
+Rails.application.config.assets.precompile += %w( deliveries.js )
+Rails.application.config.assets.precompile += %w( employees.css )
+Rails.application.config.assets.precompile += %w( employees.js )
+Rails.application.config.assets.precompile += %w( forms.css )
+Rails.application.config.assets.precompile += %w( forms.js )
+Rails.application.config.assets.precompile += %w( inwards.css )
+Rails.application.config.assets.precompile += %w( inwards.js )
+Rails.application.config.assets.precompile += %w( mail.css )
+Rails.application.config.assets.precompile += %w( mail.js )
+Rails.application.config.assets.precompile += %w( maps.css )
+Rails.application.config.assets.precompile += %w( maps.js )
+Rails.application.config.assets.precompile += %w( products.css )
+Rails.application.config.assets.precompile += %w( products.js )
+Rails.application.config.assets.precompile += %w( reports.css )
+Rails.application.config.assets.precompile += %w( reports.js )
+Rails.application.config.assets.precompile += %w( returns.js )
+Rails.application.config.assets.precompile += %w( returns.css )
+Rails.application.config.assets.precompile += %w( vendors.js )
+Rails.application.config.assets.precompile += %w( vendors.css )
+Rails.application.config.assets.precompile += %w( base.css )
+Rails.application.config.assets.precompile += %w( base.js )
+
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+#Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
