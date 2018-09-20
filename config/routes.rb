0a43d6888740fae2c0fb991245c0d854b962d977
komya-21 <komalmane21@gmail.com>
2018-09-20 12:39:12 +0530

    removed user

--- config/routes.rb
+++ config/routes.rb
@@ -144,7 +144,6 @@ end
   resources :companies
   get 'home/dashboard'
 
-  devise_for :users
   root 'home#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
