ce36a4d52781fde4cc6ddfbeeb1ba0981ae49451
komya-21 <komalmane21@gmail.com>
2018-09-20 13:30:19 +0530

    added in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+
+before_save :ensure_authentication_token
+def skip_confirmation!
+  self.confirmed_at = Time.now
+end
 end
