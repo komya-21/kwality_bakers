3f7b54fff346a32d9e3a8be6cd6be1590b40c68f
komya-21 <komalmane21@gmail.com>
2018-09-20 12:54:20 +0530

    added migrations

--- db/migrate/20180430072205_devise_create_users.rb
+++ db/migrate/20180430072205_devise_create_users.rb
@@ -39,11 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_column :users, :authentication_token, :text
-    add_column :users, :authentication_token_created_at, :datetime
-
-    add_index :users, :authentication_token, unique: true
-
+    
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180917124104) do
+ActiveRecord::Schema.define(version: 20180920071718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -356,9 +356,15 @@ ActiveRecord::Schema.define(version: 20180917124104) do
     t.datetime "updated_at", null: false
     t.text "authentication_token"
     t.datetime "authentication_token_created_at"
+    t.bigint "vendor_id"
+    t.bigint "employee_id"
+    t.bigint "location_id"
     t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["employee_id"], name: "index_users_on_employee_id"
+    t.index ["location_id"], name: "index_users_on_location_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["vendor_id"], name: "index_users_on_vendor_id"
   end
 
   create_table "vendors", force: :cascade do |t|
@@ -483,6 +489,9 @@ ActiveRecord::Schema.define(version: 20180917124104) do
   add_foreign_key "returns", "products"
   add_foreign_key "returns", "vendors"
   add_foreign_key "transaction_details", "inwards"
+  add_foreign_key "users", "employees"
+  add_foreign_key "users", "locations"
+  add_foreign_key "users", "vendors"
   add_foreign_key "vendors", "locations"
   add_foreign_key "work_orders", "colors"
   add_foreign_key "workorders", "colors"
