3777f4eb32e231a26c097d0b92b050f117569118
komya-21 <komalmane21@gmail.com>
2018-09-21 15:23:03 +0530

    form changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -56,6 +56,8 @@ Rails.application.config.assets.precompile += %w( payslips.js )
 Rails.application.config.assets.precompile += %w( suppliers.css )
 Rails.application.config.assets.precompile += %w( suppliers.js )
 
+
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
