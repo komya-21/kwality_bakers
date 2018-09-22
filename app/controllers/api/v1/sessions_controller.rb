76dc90838ded0520f3a8e2d250e7559d7924cb73
komya-21 <komalmane21@gmail.com>
2018-09-22 12:02:43 +0530

    changes in sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::SessionsController < Devise::SessionsController
-  
+   skip_before_action :verify_signed_out_user
   skip_before_action :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
   respond_to :json
-  skip_before_action :verify_signed_out_user
+ 
 
   def create
    
