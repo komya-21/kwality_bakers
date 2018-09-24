0724ece0da5db46cc2bd65f8ba5626c58e7b2fc5
komya-21 <komalmane21@gmail.com>
2018-09-24 18:52:16 +0530

    removed byebug

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::SessionsController < Devise::SessionsController
    skip_before_action :verify_signed_out_user
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token,  :if => Proc.new { |c| c.request.format == 'application/json' }
                      
 
   respond_to :json
  
 
   def create
-   byebug
+    
     warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
     render :status => 200,
            :json => { :success => true,
